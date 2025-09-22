e54b3d25c43b551ff6c5ddd6916e1c963d7a8d06
Tanuj <Tanuj_Sharma@comcast.com>
2025-09-22 16:01:03 +0530

    Added plugin for Barton Matter

---
+++ BartonMatter/BartonMatterPlugin.cpp
@@ -0,0 +1,103 @@
+/**
+* If not stated otherwise in this file or this component's LICENSE
+* file the following copyright and licenses apply:
+*
+* Copyright 2020 RDK Management
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+**/
+
+#include "BartonMatterPlugin.h"
+
+const string WPEFramework::Plugin::BartonMatter::SERVICE_NAME = "org.rdk.BartonMatter";
+
+using namespace std;
+
+namespace WPEFramework {
+    namespace Plugin {
+
+        SERVICE_REGISTRATION(BartonMatter, 1, 0);
+
+        BartonMatter* BartonMatter::_instance = nullptr;
+
+        BartonMatter::BartonMatter()
+            : PluginHost::IPlugin(), PluginHost::JSONRPC(), mService(nullptr), mBartonMatter(nullptr)
+        {
+            BartonMatter::_instance = this;
+        }
+
+        BartonMatter::~BartonMatter()
+        {
+        }
+
+        const string BartonMatter::Initialize(PluginHost::IShell* service )
+        {
+            string message;
+
+            ASSERT(mService == nullptr);
+            ASSERT(mBartonMatter == nullptr);
+
+            mConnectionId = 0;
+            mService = service;
+
+            mBartonMatter = mService->Root<Exchange::IBartonMatter>(mConnectionId, 5000, _T("BartonMatterImplementation"));
+
+            if (mBartonMatter == nullptr)
+            {
+                message = _T("BartonMatter implementation could not be instantiated.");
+                mService = nullptr;
+                return message;
+            }
+            std::string waylandDisplay("");
+            mConfig.FromString(service->ConfigLine());
+            if (mConfig.Display.IsSet() == true)
+            {
+                waylandDisplay = mConfig.Display.Value();
+            }
+            else
+            {
+                const char* display = getenv("WAYLAND_DISPLAY");
+                if (NULL != display)
+                {
+                   waylandDisplay = display;
+                }
+            }
+            mBartonMatter->Initialize(waylandDisplay);
+	    Exchange::JBartonMatter::Register(*this, mBartonMatter);
+            return "";
+        }
+
+        void BartonMatter::Deinitialize(PluginHost::IShell* /* service */)
+        {
+	    LOGINFO("Deinitializing BartonMatter instance");	
+            if (mBartonMatter != nullptr) {
+
+                Exchange::JBartonMatter::Unregister(*this);
+                mBartonMatter->Deinitialize();
+
+                mBartonMatter->Release();
+                mBartonMatter = nullptr;
+                mService = nullptr;
+                BartonMatter::_instance = nullptr;
+                LOGINFO("BartonMatter deinitialized successfully");
+
+            }
+	}
+
+        string BartonMatter::Information() const
+        {
+            return(string("{\"service\": \"") + SERVICE_NAME + string("\"}"));
+        }
+        
+    } // namespace Plugin
+} // namespace WPEFramework
---
+++ BartonMatter/BartonMatterPlugin.h
@@ -0,0 +1,88 @@
+/**
+* If not stated otherwise in this file or this component's LICENSE
+* file the following copyright and licenses apply:
+*
+* Copyright 2020 RDK Management
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+**/
+
+#pragma once
+
+#include "Module.h"
+#include "UtilsLogging.h"
+#include "UtilsJsonRpc.h"
+#include <interfaces/json/JsonData_BartonMatter.h>
+#include <interfaces/json/JBartonMatter.h>
+#include <interfaces/IBartonMatter.h>
+
+namespace WPEFramework {
+
+    namespace Plugin {
+
+        class BartonMatter : public PluginHost::IPlugin, public PluginHost::JSONRPC {
+        public:
+            class Config : public Core::JSON::Container
+            {
+                private:
+                    Config(const Config&) = delete;
+                    Config& operator=(const Config&) = delete;
+
+                public:
+                    Config()
+                        : Core::JSON::Container()
+                          , Display(_T(""))
+                    {
+                        Add(_T("embedthunderjs"), &EmbedThunderJS);
+                        Add(_T("clientidentifier"), &Display);
+                    }
+                    ~Config()
+                    {
+                    }
+
+                public:
+                    Core::JSON::Boolean EmbedThunderJS;
+                    Core::JSON::String Display;
+            };
+
+            BEGIN_INTERFACE_MAP(BartonMatterPlugin)
+            INTERFACE_AGGREGATE(Exchange::IBartonMatter, mBartonMatter)
+	    INTERFACE_ENTRY(PluginHost::IPlugin)
+	    INTERFACE_ENTRY(PluginHost::IDispatcher)
+	    END_INTERFACE_MAP
+
+        public:
+            BartonMatter();
+            virtual ~BartonMatter();
+            virtual const string Initialize(PluginHost::IShell* service) override;
+            virtual void Deinitialize(PluginHost::IShell* service) override;
+            virtual string Information() const override;
+
+        public/*members*/:
+            static BartonMatter* _instance;
+
+        public /*constants*/:
+            static const string SERVICE_NAME;
+
+        private/*registered methods (wrappers)*/:
+
+            BartonMatter(const BartonMatter&) = delete;
+            BartonMatter& operator=(const BartonMatter&) = delete;
+
+            PluginHost::IShell* mService;
+            uint32_t mConnectionId;
+            Exchange::IBartonMatter* mBartonMatter;
+            Config mConfig;
+        };
+    } // namespace Plugin
+} // namespace WPEFramework
