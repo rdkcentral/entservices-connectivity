8b5ee16cf1cf19f94153b41ca9bda1c048df5c0b
Gangadasari <rganga607@apac.comcast.com>
2026-02-18 23:38:16 +0530

    add calculator in the cov.sh file

--- cov_build.sh
+++ cov_build.sh
@@ -23,6 +23,7 @@ cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-connectivity \
 -DRDK_SERVICES_L1_TEST=ON \
 -DDS_FOUND=ON \
 -DPLUGIN_BLUETOOTH=ON \
+-DPLUGIN_CALCULATOR=ON \
 -DCMAKE_CXX_FLAGS="-DEXCEPTIONS_ENABLE=ON \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers/audiocapturemgr \
