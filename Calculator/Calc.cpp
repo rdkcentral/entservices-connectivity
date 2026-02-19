b4d39cb83f3bdaaf505ce0138c518ac48c3e9329
rganga607 <RevanthReddy_Gangadasari@comcast.com>
2026-02-19 11:41:48 +0530

    Update Calc.cpp
    Removed getNumberParameterObject() function

--- Calculator/Calc.cpp
+++ Calculator/Calc.cpp
@@ -1,17 +1,18 @@
 /**
-* Calculator Plugin Implementation
-*/
- 
+ * Calculator Plugin Implementation
+ */
+
 #include "Calc.h"
- 
+#include <cmath>
+
 #define API_VERSION_NUMBER_MAJOR 1
 #define API_VERSION_NUMBER_MINOR 0
 #define API_VERSION_NUMBER_PATCH 0
- 
+
 using namespace WPEFramework;
- 
+
 namespace WPEFramework {
- 
+
 namespace {
     static Plugin::Metadata<Plugin::Calc> metadata(
         API_VERSION_NUMBER_MAJOR,
@@ -22,18 +23,18 @@ namespace {
         {}
     );
 }
- 
+
 namespace Plugin {
- 
+
 SERVICE_REGISTRATION(Calc,
                      API_VERSION_NUMBER_MAJOR,
                      API_VERSION_NUMBER_MINOR,
                      API_VERSION_NUMBER_PATCH);
- 
-// Service name
+
+// Service Name
 const string Calc::SERVICE_NAME = "org.rdk.Calculator";
- 
-// Method names
+
+// Method Names
 const string Calc::METHOD_ADD = "add";
 const string Calc::METHOD_SUBTRACT = "subtract";
 const string Calc::METHOD_MULTIPLY = "multiply";
@@ -42,9 +43,9 @@ const string Calc::METHOD_MODULUS = "modulus";
 const string Calc::METHOD_POWER = "power";
 const string Calc::METHOD_SQRT = "sqrt";
 const string Calc::METHOD_GET_API_VERSION_NUMBER = "getApiVersionNumber";
- 
+
 ////////////////////////////////////////////////////////////
- 
+
 Calc::Calc()
     : PluginHost::JSONRPC()
     , m_apiVersionNumber(API_VERSION_NUMBER_MAJOR)
@@ -58,144 +59,155 @@ Calc::Calc()
     Register(METHOD_POWER, &Calc::powerWrapper, this);
     Register(METHOD_SQRT, &Calc::sqrtWrapper, this);
 }
- 
+
 Calc::~Calc() {}
- 
+
 ////////////////////////////////////////////////////////////
- 
+
 void Calc::Deinitialize(PluginHost::IShell* service)
 {
     UNUSED(service);
 }
- 
+
 string Calc::Information() const
 {
     return string("{\"service\": \"") + SERVICE_NAME + "\"}";
 }
- 
+
 ////////////////////////////////////////////////////////////
 //////////////////// INTERNAL LOGIC ////////////////////////
 ////////////////////////////////////////////////////////////
- 
+
 double Calc::add(double a, double b) {
     return a + b;
 }
- 
+
 double Calc::subtract(double a, double b) {
     return a - b;
 }
- 
+
 double Calc::multiply(double a, double b) {
     return a * b;
 }
- 
+
 double Calc::divide(double a, double b) {
     return (b != 0) ? a / b : 0;
 }
- 
+
 int Calc::modulus(int a, int b) {
     return (b != 0) ? a % b : 0;
 }
- 
+
 double Calc::power(double a, double b) {
     return std::pow(a, b);
 }
- 
+
 double Calc::sqrtValue(double a) {
     return std::sqrt(a);
 }
- 
+
 ////////////////////////////////////////////////////////////
 //////////////////// WRAPPER METHODS ///////////////////////
 ////////////////////////////////////////////////////////////
- 
+
 uint32_t Calc::getApiVersionNumber(const JsonObject& parameters, JsonObject& response)
 {
     UNUSED(parameters);
     response["version"] = m_apiVersionNumber;
     returnResponse(true);
 }
- 
+
 uint32_t Calc::addWrapper(const JsonObject& parameters, JsonObject& response)
 {
-    double a, b;
-    getNumberParameterObject(parameters, "a", a);
-    getNumberParameterObject(parameters, "b", b);
- 
+    if (!parameters.HasLabel("a") || !parameters.HasLabel("b"))
+        returnResponse(false);
+
+    double a = parameters["a"].Number();
+    double b = parameters["b"].Number();
+
     response["result"] = add(a, b);
     returnResponse(true);
 }
- 
+
 uint32_t Calc::subtractWrapper(const JsonObject& parameters, JsonObject& response)
 {
-    double a, b;
-    getNumberParameterObject(parameters, "a", a);
-    getNumberParameterObject(parameters, "b", b);
- 
+    if (!parameters.HasLabel("a") || !parameters.HasLabel("b"))
+        returnResponse(false);
+
+    double a = parameters["a"].Number();
+    double b = parameters["b"].Number();
+
     response["result"] = subtract(a, b);
     returnResponse(true);
 }
- 
+
 uint32_t Calc::multiplyWrapper(const JsonObject& parameters, JsonObject& response)
 {
-    double a, b;
-    getNumberParameterObject(parameters, "a", a);
-    getNumberParameterObject(parameters, "b", b);
- 
+    if (!parameters.HasLabel("a") || !parameters.HasLabel("b"))
+        returnResponse(false);
+
+    double a = parameters["a"].Number();
+    double b = parameters["b"].Number();
+
     response["result"] = multiply(a, b);
     returnResponse(true);
 }
- 
+
 uint32_t Calc::divideWrapper(const JsonObject& parameters, JsonObject& response)
 {
-    double a, b;
-    getNumberParameterObject(parameters, "a", a);
-    getNumberParameterObject(parameters, "b", b);
- 
-    if (b == 0) {
+    if (!parameters.HasLabel("a") || !parameters.HasLabel("b"))
         returnResponse(false);
-    }
- 
+
+    double a = parameters["a"].Number();
+    double b = parameters["b"].Number();
+
+    if (b == 0)
+        returnResponse(false);
+
     response["result"] = divide(a, b);
     returnResponse(true);
 }
- 
+
 uint32_t Calc::modulusWrapper(const JsonObject& parameters, JsonObject& response)
 {
-    int a, b;
-    getNumberParameterObject(parameters, "a", a);
-    getNumberParameterObject(parameters, "b", b);
- 
-    if (b == 0) {
+    if (!parameters.HasLabel("a") || !parameters.HasLabel("b"))
         returnResponse(false);
-    }
- 
+
+    int a = static_cast<int>(parameters["a"].Number());
+    int b = static_cast<int>(parameters["b"].Number());
+
+    if (b == 0)
+        returnResponse(false);
+
     response["result"] = modulus(a, b);
     returnResponse(true);
 }
- 
+
 uint32_t Calc::powerWrapper(const JsonObject& parameters, JsonObject& response)
 {
-    double a, b;
-    getNumberParameterObject(parameters, "a", a);
-    getNumberParameterObject(parameters, "b", b);
- 
+    if (!parameters.HasLabel("a") || !parameters.HasLabel("b"))
+        returnResponse(false);
+
+    double a = parameters["a"].Number();
+    double b = parameters["b"].Number();
+
     response["result"] = power(a, b);
     returnResponse(true);
 }
- 
+
 uint32_t Calc::sqrtWrapper(const JsonObject& parameters, JsonObject& response)
 {
-    double a;
-    getNumberParameterObject(parameters, "a", a);
- 
-    if (a < 0) {
+    if (!parameters.HasLabel("a"))
         returnResponse(false);
-    }
- 
+
+    double a = parameters["a"].Number();
+
+    if (a < 0)
+        returnResponse(false);
+
     response["result"] = sqrtValue(a);
     returnResponse(true);
 }
- 
+
 } // Plugin
 } // WPEFramework
